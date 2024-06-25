21c2b826b58c03a1cd85a9e5b4e2fbce465da1f4
Rodion Efremov <coderodd3@yandex.com>
2024-06-25 21:49:24 +0300

    ...

--- src/main/java/com/github/coderodde/game/chess/ChessBoardState.java
+++ src/main/java/com/github/coderodde/game/chess/ChessBoardState.java
@@ -14,99 +14,70 @@ public final class ChessBoardState {
     
     private static final int N = 8;
     
-    public static final byte EMPTY        = 0;
-    public static final byte WHITE_PAWN   = 1;
-    public static final byte WHITE_BISHOP = 2;
-    public static final byte WHITE_KNIGHT = 3;
-    public static final byte WHITE_ROOK   = 4;
-    public static final byte WHITE_QUEEN  = 5;
-    public static final byte WHITE_KING   = 6;
+    public static final int EMPTY        = 0;
+    public static final int WHITE_PAWN   = 1;
+    public static final int WHITE_BISHOP = 2;
+    public static final int WHITE_KNIGHT = 3;
+    public static final int WHITE_ROOK   = 4;
+    public static final int WHITE_QUEEN  = 5;
+    public static final int WHITE_KING   = 6;
  
-    public static final byte BLACK_PAWN   = 9;
-    public static final byte BLACK_BISHOP = 10;
-    public static final byte BLACK_KNIGHT = 11;
-    public static final byte BLACK_ROOK   = 12;
-    public static final byte BLACK_QUEEN  = 13;
-    public static final byte BLACK_KING   = 14;
+    public static final int BLACK_PAWN   = 9;
+    public static final int BLACK_BISHOP = 10;
+    public static final int BLACK_KNIGHT = 11;
+    public static final int BLACK_ROOK   = 12;
+    public static final int BLACK_QUEEN  = 13;
+    public static final int BLACK_KING   = 14;
     
-    private static final int CELL_COLOR_NONE = 0;
-    private static final int CELL_COLOR_WHITE = 1;
+    private static final int CELL_COLOR_NONE  = 0;
+    private static final int CELL_COLOR_WHITE = +1;
     private static final int CELL_COLOR_BLACK = -1;
     
-    private byte[][] state = new byte[N][N];
-    private byte[] promotionCounters = new byte[8];
-    private byte whitePawnInitialFlags = (byte) 0xff;
-    private byte blackPawnInitialFlags = (byte) 0xff;
-     
-    private static final int PROMOTION_COUNTER_INDEX_WHITE_QUEEN  = 0;
-    private static final int PROMOTION_COUNTER_INDEX_WHITE_ROOK   = 1;
-    private static final int PROMOTION_COUNTER_INDEX_WHITE_BISHOP = 2;
-    private static final int PROMOTION_COUNTER_INDEX_WHITE_KNIGHT = 3;
-     
-    private static final int PROMOTION_COUNTER_INDEX_BLACK_QUEEN  = 4;
-    private static final int PROMOTION_COUNTER_INDEX_BLACK_ROOK   = 5;
-    private static final int PROMOTION_COUNTER_INDEX_BLACK_BISHOP = 6;
-    private static final int PROMOTION_COUNTER_INDEX_BLACK_KNIGHT = 7;
+    private int[][] state = new int[N][N];
+    private boolean[] whitePawnInitialFlags = new boolean[N];
+    private boolean[] blackPawnInitialFlags = new boolean[N];
+    private boolean[] whiteIsPreviouslyDoubleMoved = new boolean[N];
+    private boolean[] blackIsPreviouslyDoubleMoved = new boolean[N];
     
     public ChessBoardState() {
         
         // Black pieces:
-        set(0, 0, BLACK_ROOK);
-        set(7, 0, BLACK_ROOK);
-//        state[0][0] =
-//        state[0][7] = BLACK_ROOK;
+        state[0][0] =
+        state[0][7] = BLACK_ROOK;
   
-        set(1, 0, BLACK_KNIGHT);
-        set(6, 0, BLACK_KNIGHT);
-//        state[0][1] = 
-//        state[0][6] = BLACK_KNIGHT;
-  
-        set(2, 0, BLACK_BISHOP);
-        set(5, 0, BLACK_BISHOP);
+        state[0][1] = 
+        state[0][6] = BLACK_KNIGHT;
         
-//        state[0][2] = 
-//        state[0][5] = BLACK_BISHOP;
+        state[0][2] = 
+        state[0][5] = BLACK_BISHOP;
   
-        set(3, 0, BLACK_QUEEN);
-        set(4, 0, BLACK_KING);
-//        state[0][3] = BLACK_QUEEN;
-//        state[0][4] = BLACK_KING;
+        state[0][3] = BLACK_QUEEN;
+        state[0][4] = BLACK_KING;
         
         for (int x = 0; x < N; x++) {
-            set(x, 1, BLACK_PAWN);
-//            state[1][x] = BLACK_PAWN;
+            state[1][x] = BLACK_PAWN;
         }
         
         // White pieces:
-        set(0, 7, WHITE_ROOK);
-        set(7, 7, WHITE_ROOK);
-//        state[7][0] =
-//        state[7][7] = WHITE_ROOK;
+        state[7][0] =
+        state[7][7] = WHITE_ROOK;
   
-        set(1, 7, WHITE_KNIGHT);
-        set(6, 7, WHITE_KNIGHT);
-//        state[7][1] = 
-//        state[7][6] = WHITE_KNIGHT;
+        state[7][1] = 
+        state[7][6] = WHITE_KNIGHT;
         
-        set(2, 7, WHITE_BISHOP);
-        set(5, 7, WHITE_BISHOP);
-//        state[7][2] = 
-//        state[7][5] = WHITE_BISHOP;
-  
-        set(3, 7, WHITE_QUEEN);
-        set(4, 7, WHITE_KING);
+        state[7][2] = 
+        state[7][5] = WHITE_BISHOP;
         
-//        state[7][3] = WHITE_QUEEN;
-//        state[7][4] = WHITE_KING;
+        state[7][3] = WHITE_QUEEN;
+        state[7][4] = WHITE_KING;
         
         for (int x = 0; x < N; x++) {
-            set(x, 6, WHITE_PAWN);
-//            state[6][x] = WHITE_PAWN;
+            state[6][x] = WHITE_PAWN;
         }
     }
     
     public ChessBoardState(final ChessBoardState copy) {
-        this.state = new byte[N][N];
+        this.state = new int[N][N];
         
         for (int y = 0; y < N; y++) {
             System.arraycopy(copy.state[y], 0, this.state[y], 0, N);
@@ -114,8 +85,8 @@ public final class ChessBoardState {
         
         this.whitePawnInitialFlags = copy.whitePawnInitialFlags;
         this.blackPawnInitialFlags = copy.blackPawnInitialFlags;
-        this.promotionCounters = Arrays.copyOf(copy.promotionCounters, 
-                                               copy.promotionCounters.length);
+        this.whiteIsPreviouslyDoubleMoved = copy.whiteIsPreviouslyDoubleMoved;
+        this.blackIsPreviouslyDoubleMoved = copy.blackIsPreviouslyDoubleMoved;
     }
     
     @Override
@@ -146,7 +117,7 @@ public final class ChessBoardState {
      * Clears the entire board. Used in unit testing.
      */
     public void clear() {
-        this.state = new byte[N][N];
+        this.state = new int[N][N];
     }
     
     /**
@@ -158,7 +129,7 @@ public final class ChessBoardState {
      * 
      * @return the piece value.
      */
-    public byte get(final int x, final int y) {
+    public int get(final int x, final int y) {
         return state[y][x];
     }
     
@@ -171,12 +142,13 @@ public final class ChessBoardState {
      * 
      * @param piece the value of the desired piece.
      */
-    public void set(final int x, final int y, final byte piece) {
+    public void set(final int x, final int y, final int piece) {
         state[y][x] = piece;
     }
     
     /**
-     * Clears the position at rank {@code y} and file {@code x}.
+     * Clears the position at rank {@code y} and file {@code x}. Used in unit 
+     * testing.
      * 
      * @param x the file of the requested piece.
      * @param y the rank of the requested piece.
@@ -251,7 +223,7 @@ public final class ChessBoardState {
                             final int x,
                             final int y) {
         
-        final byte cell = state[y][x];
+        final int cell = state[y][x];
         
         switch (cell) {
             case WHITE_PAWN:
@@ -307,11 +279,24 @@ public final class ChessBoardState {
             // Once here, can move the white pawn two moves forward:
             final ChessBoardState child = new ChessBoardState(this);
 
-            child.unsetWhiteInitialMovePawn(x);
+            child.markWhiteInitialMovePawn(x);
             child.state[6][x] = EMPTY; 
             child.state[4][x] = WHITE_PAWN;
             
             children.add(child);
+        }
+        
+        if (y == 3) {
+            // Try en passant, white pawn can capture a black onen?
+            if (x > 0) {
+                // Try en passant to the left:
+                enPassantWhitePawn(x - 1, children);
+            }
+            
+            if (x < N - 1) {
+                // Try en passant to the right:
+                enPassantWhitePawn(x + 1, children);
+            }
         }
         
         if (state[y - 1][x] == EMPTY && y == 1) {
@@ -352,6 +337,28 @@ public final class ChessBoardState {
             
             children.add(child);
         }
+    }
+    
+    /**
+     * Tries to perform an en passant by the white pawn at the file {@code x} 
+     * to a black pawn at the file {@code x - 1}.
+     * 
+     * @param x        the file of the capturing white pawn.
+     * @param children the list of child n
+     */
+    private void enPassantWhitePawnToLeft(
+            final int x, 
+            final List<ChessBoardState> children) {
+        
+        if (!blackIsPreviouslyDoubleMoved[x]) {
+            return;
+        }
+        
+        final ChessBoardState child = new ChessBoardState(this);
+        
+        child.clear(x, x);
+        
+        children.add(child);
     }
     
     private void expandImplBlackPawn(final List<ChessBoardState> children,
@@ -416,7 +423,7 @@ public final class ChessBoardState {
      * @return {@code true} iff the white pawn in question has not moved yet.
      */
     private boolean checkWhiteInitialMovePawn(final int x) {
-        return (whitePawnInitialFlags & (1 << x)) != 0;
+        return whitePawnInitialFlags[x] == false;
     }
     
     /**
@@ -424,8 +431,8 @@ public final class ChessBoardState {
      * 
      * @param x the file index of the white pawn.
      */
-    private void unsetWhiteInitialMovePawn(final int x) {
-        whitePawnInitialFlags &= ~(1 << x);
+    private void markWhiteInitialMovePawn(final int x) {
+        whitePawnInitialFlags[x] = true;
     }
     
     /**
@@ -436,7 +443,7 @@ public final class ChessBoardState {
      * @return {@code true} iff the black pawn in question has not moved yet.
      */
     private boolean checkBlackInitialMovePawn(final int x) {
-        return (blackPawnInitialFlags & (1 << x)) != 0;
+        return blackPawnInitialFlags[x] == false;
     }
     
     /**
@@ -445,80 +452,14 @@ public final class ChessBoardState {
      * @param x the file index of the black pawn.
      */
     private void unsetBlackInitialMovePawn(final int x) {
-        blackPawnInitialFlags &= ~(1 << x);
-    }
-    
-    /**
-     * Converts the piece code to its textual representation. Used in
-     * {@link #toString()}.
-     * 
-     * @param piece the piece code.
-     * 
-     * @return textual representation of the input piece code.
-     */
-    private static char pieceToChar(final byte piece) {
-        switch (piece) {
-            case EMPTY -> {
-                return '.';
-            }
-                
-            case WHITE_PAWN -> {
-                return 'P';
-            }
-                
-            case BLACK_PAWN -> {
-                return 'p';
-            }
-                
-            case WHITE_ROOK -> {
-                return 'R';
-            }
-                
-            case BLACK_ROOK -> {
-                return 'r';
-            }
-                
-            case WHITE_BISHOP -> {
-                return 'B';
-            }
-                
-            case BLACK_BISHOP -> {
-                return 'b';
-            }
-                
-            case WHITE_KNIGHT -> {
-                return 'K';
-            }
-                
-            case BLACK_KNIGHT -> {
-                return 'k';
-            }
-                
-            case WHITE_QUEEN -> {
-                return 'Q';
-            }
-                
-            case BLACK_QUEEN -> {
-                return 'q';
-            }
-                
-            case WHITE_KING -> {
-                return 'X';
-            }
-                
-            case BLACK_KING -> {
-                return 'x';
-            }
-                
-            default -> throw new IllegalStateException();
-        }
+        blackPawnInitialFlags[x] = true;
     }
     
     /**
      * Returns the color of the cell at file {@code (x + 1)} and rank 
      * {@code 8 - y}.
      * 
-     * @param x tje file index.
+     * @param x the file index.
      * @param y the rank index.
      * 
      * @return {@link #CELL_COLOR_NONE} if the requested cell is empty,
@@ -526,19 +467,18 @@ public final class ChessBoardState {
      *         {@link #CELL_COLOR_BLACK} if the requested cell is black.
      */
     private int getCellColor(final int x, final int y) {
-        final byte cell = state[y][x];
+        final int cell = state[y][x];
         
         if (cell == EMPTY) {
             return CELL_COLOR_NONE;
         }
         
-        return 0 < cell && cell < 7 ? 
-                    CELL_COLOR_WHITE : 
-                    CELL_COLOR_BLACK;
+        return 0 < cell && cell < 7 ? CELL_COLOR_WHITE : 
+                                      CELL_COLOR_BLACK;
     }
     
     private char convertPieceCodeToUnicodeCharacter(final int x, final int y) {
-        final byte pieceCode = state[y][x];
+        final int pieceCode = state[y][x];
         
         switch (pieceCode) {
             case EMPTY -> {
