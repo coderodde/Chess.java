5b25715837edd13e3ed70bec0c6825cdd5b24588
Rodion Efremov <coderodd3@yandex.com>
2024-06-26 12:44:28 +0300

    Working on black pawn expansion.

--- src/test/java/com/github/coderodde/game/chess/ChessBoardStateTest.java
+++ src/test/java/com/github/coderodde/game/chess/ChessBoardStateTest.java
@@ -5,8 +5,10 @@ import static com.github.coderodde.game.chess.ChessBoardState.BLACK_KNIGHT;
 import static com.github.coderodde.game.chess.ChessBoardState.BLACK_PAWN;
 import static com.github.coderodde.game.chess.ChessBoardState.BLACK_ROOK;
 import static com.github.coderodde.game.chess.ChessBoardState.WHITE_BISHOP;
+import static com.github.coderodde.game.chess.ChessBoardState.WHITE_KING;
 import static com.github.coderodde.game.chess.ChessBoardState.WHITE_PAWN;
 import static com.github.coderodde.game.chess.ChessBoardState.WHITE_QUEEN;
+import static com.github.coderodde.game.chess.ChessBoardState.WHITE_ROOK;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -215,5 +217,85 @@ public final class ChessBoardStateTest {
         move.set(7, 2, WHITE_PAWN);
         
         assertTrue(children.contains(move));
+    }
+    
+    @Test
+    public void moveBlackPawnInitialDoubleMove() {
+        state.set(3, 1, BLACK_PAWN);
+        
+        final List<ChessBoardState> children = state.expand(PlayerTurn.BLACK);
+        
+        assertEquals(2, children.size());
+        
+        final ChessBoardState child1 = children.get(0);
+        final ChessBoardState child2 = children.get(1);
+        
+        assertTrue(children.contains(child1));
+        assertTrue(children.contains(child2));
+        
+        final Set<Integer> indexSet = new HashSet<>();
+        
+        indexSet.add(children.indexOf(child1));
+        indexSet.add(children.indexOf(child2));
+        
+        assertEquals(2, indexSet.size());
+        
+        final ChessBoardState move1 = new ChessBoardState();
+        final ChessBoardState move2 = new ChessBoardState();
+        
+        move1.clear();
+        move2.clear();
+        
+        move1.set(3, 2, BLACK_PAWN);
+        move2.set(3, 3, BLACK_PAWN);
+        
+        assertTrue(children.contains(move1));
+        assertTrue(children.contains(move2));
+    }
+    
+    @Test
+    public void blackPawnCannotMoveForward() {
+        state.set(4, 2, BLACK_PAWN);
+        state.set(4, 3, WHITE_ROOK);
+        
+        final List<ChessBoardState> children = state.expand(PlayerTurn.WHITE);
+        
+        assertTrue(children.isEmpty());
+    }
+    
+    @Test
+    public void blackPawnCanEatBothDirectionsAndMoveForward() {
+        state.set(5, 3, BLACK_PAWN);
+        state.set(4, 4, WHITE_ROOK);
+        state.set(6, 4, WHITE_KING);
+        
+        final List<ChessBoardState> children = state.expand(PlayerTurn.BLACK);
+        
+        assertEquals(3, children.size());
+        
+        final ChessBoardState move1 = new ChessBoardState();
+        final ChessBoardState move2 = new ChessBoardState();
+        final ChessBoardState move3 = new ChessBoardState();
+        
+        move1.clear();
+        move2.clear();
+        move3.clear();
+        
+        // Capture to the left:
+        move1.set(4, 4, BLACK_PAWN);
+        move1.set(6, 4, WHITE_KING);
+        
+        // Move forward:
+        move2.set(4, 4, WHITE_ROOK);
+        move2.set(5, 4, BLACK_PAWN);
+        move2.set(6, 4, WHITE_KING);
+        
+        // Capture to the right:
+        move3.set(6, 4, BLACK_PAWN);
+        move3.set(4, 4, WHITE_ROOK);
+        
+        assertTrue(children.contains(move1));
+        assertTrue(children.contains(move2));
+        assertTrue(children.contains(move3));
     }
 }
