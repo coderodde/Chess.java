222f40fe8673c7afc0898dddf00794ab9004989b
Rodion Efremov <coderodd3@gmail.com>
2024-06-26 22:19:13 +0300

    ...

--- src/main/java/com/github/coderodde/game/chess/Chess.java
+++ src/main/java/com/github/coderodde/game/chess/Chess.java
@@ -1,8 +1,22 @@
 package com.github.coderodde.game.chess;
 
+import static com.github.coderodde.game.chess.ChessBoardState.WHITE_PAWN;
+import java.util.List;
+
 public class Chess {
 
     public static void main(String[] args) {
-        System.out.println(new ChessBoardState());
+        ChessBoardState s = new ChessBoardState();
+        s.clear();
+        
+        s.set(2, 6, WHITE_PAWN);
+        s.set(4, 6, WHITE_PAWN);
+        
+        final List<ChessBoardState> children = s.expand(PlayerTurn.WHITE);
+        
+        for (final ChessBoardState child : children) {
+            System.out.println(child);
+            System.out.println();
+        }
     }
 }
