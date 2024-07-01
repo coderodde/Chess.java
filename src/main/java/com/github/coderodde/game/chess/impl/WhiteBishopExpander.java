65dcc888d9d9429296945fefc220e2fa01861f5d
Rodion Efremov <coderodd3@gmail.com>
2024-07-01 06:59:37 +0300

    Working on WhiteBishopExpander.java.

--- src/main/java/com/github/coderodde/game/chess/impl/WhiteBishopExpander.java
+++ src/main/java/com/github/coderodde/game/chess/impl/WhiteBishopExpander.java
@@ -3,6 +3,7 @@ package com.github.coderodde.game.chess.impl;
 import com.github.coderodde.game.chess.AbstractChessBoardStateExpander;
 import com.github.coderodde.game.chess.CellType;
 import com.github.coderodde.game.chess.ChessBoardState;
+import static com.github.coderodde.game.chess.ChessBoardState.N;
 import com.github.coderodde.game.chess.Piece;
 import java.util.List;
 
@@ -15,9 +16,28 @@ public class WhiteBishopExpander extends AbstractChessBoardStateExpander {
                        final int rank, 
                        final List<ChessBoardState> children) {
      
+        // Generate children upwards to the left:
         tryGenerateNorthWest(state,
                              file,
                              rank, 
+                             children);
+        
+        // Generate children upwards to the right:
+        tryGenerateNorthEast(state,
+                             file,
+                             rank,
+                             children);
+        
+        // Generate children downwards to the left:
+        tryGenerateSouthWest(state,
+                             file, 
+                             rank,
+                             children);
+        
+        // Generate children downwards to the right:
+        tryGenerateSouthEast(state,
+                             file, 
+                             rank,
                              children);
     }
     
@@ -32,6 +52,144 @@ public class WhiteBishopExpander extends AbstractChessBoardStateExpander {
             
             final CellType cellType = state.getCellType(file - index,
                                                         rank - index);
+            
+            if (cellType == CellType.BLACK) {
+                // Can capture:
+                final ChessBoardState child = new ChessBoardState(state);
+                
+                child.set(file - index, 
+                          rank - index, 
+                          child.get(file,
+                                    rank));
+                
+                child.clear(file,
+                            rank);
+                
+                children.add(child);
+                return;
+            }
+            
+            if (cellType == CellType.WHITE) {
+                // Cannot move further because of obstruction:
+            }
+            
+            final ChessBoardState child = new ChessBoardState(state);
+            
+            child.set(file - index, 
+                      rank - index,
+                      child.get(file,
+                                rank));
+            
+            child.clear(file, 
+                        rank);
+            
+            children.add(child);
+        }
+    }
+    
+    private void tryGenerateNorthEast(final ChessBoardState state,
+                                      final int file,
+                                      final int rank,
+                                      final List<ChessBoardState> children) {
+        
+        for (int index = 1, endIndex = Math.min(N - file - 1, rank); 
+                 index <= endIndex;
+                 index++) {
+            
+            final CellType cellType = state.getCellType(file + index,
+                                                        rank - index);
+            
+            if (cellType == CellType.BLACK) {
+                // Can capture:
+                final ChessBoardState child = new ChessBoardState(state);
+                
+                child.set(file - index, 
+                          rank - index, 
+                          child.get(file,
+                                    rank));
+                
+                child.clear(file,
+                            rank);
+                
+                children.add(child);
+                return;
+            }
+            
+            if (cellType == CellType.WHITE) {
+                // Cannot move further because of obstruction:
+            }
+            
+            final ChessBoardState child = new ChessBoardState(state);
+            
+            child.set(file - index, 
+                      rank - index,
+                      child.get(file,
+                                rank));
+            
+            child.clear(file, 
+                        rank);
+            
+            children.add(child);
+        }
+    }
+    
+    private void tryGenerateSouthWest(final ChessBoardState state,
+                                      final int file,
+                                      final int rank,
+                                      final List<ChessBoardState> children) {
+                                                                 
+        for (int index = 1, endIndex = Math.min(file, N - 1 - rank); 
+                 index <= endIndex;
+                 index++) {
+            
+            final CellType cellType = state.getCellType(file - index,
+                                                        rank + index);
+            
+            if (cellType == CellType.BLACK) {
+                // Can capture:
+                final ChessBoardState child = new ChessBoardState(state);
+                
+                child.set(file - index, 
+                          rank - index, 
+                          child.get(file,
+                                    rank));
+                
+                child.clear(file,
+                            rank);
+                
+                children.add(child);
+                return;
+            }
+            
+            if (cellType == CellType.WHITE) {
+                // Cannot move further because of obstruction:
+            }
+            
+            final ChessBoardState child = new ChessBoardState(state);
+            
+            child.set(file - index, 
+                      rank - index,
+                      child.get(file,
+                                rank));
+            
+            child.clear(file, 
+                        rank);
+            
+            children.add(child);
+        }
+    }
+    
+    private void tryGenerateSouthEast(final ChessBoardState state,
+                                      final int file,
+                                      final int rank,
+                                      final List<ChessBoardState> children) {
+                                                                 
+        for (int index = 1, endIndex = Math.min(N - 1 - file, N - 1 - rank); 
+                 index <= endIndex;
+                 index++) {
+            
+            final CellType cellType = state.getCellType(file + index,
+                                                        rank + index);
             
             if (cellType == CellType.BLACK) {
                 // Can capture:
