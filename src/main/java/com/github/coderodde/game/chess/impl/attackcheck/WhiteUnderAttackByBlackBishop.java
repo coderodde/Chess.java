0bfadf1b5ffe3fac70395570fc427455f0096731
Rodion Efremov <coderodd3@gmail.com>
2024-07-08 07:34:34 +0300

    WhiteUnderAttackByBlackBishop is unit tested and covered 100%.

---
+++ src/main/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackByBlackBishop.java
@@ -0,0 +1,223 @@
+package com.github.coderodde.game.chess.impl.attackcheck;
+
+import com.github.coderodde.game.chess.ChessBoardState;
+import static com.github.coderodde.game.chess.ChessBoardState.N;
+import com.github.coderodde.game.chess.Piece;
+import com.github.coderodde.game.chess.PieceType;
+import com.github.coderodde.game.chess.UnderAttackCheck;
+
+/**
+ * This class is responsible for checking whether a white piece is threatened by
+ * a black bishop.
+ * 
+ * @version 1.0.0 (Jul 8, 2024)
+ * @since 1.0.0 (Jul 8, 2024)
+ */
+public class WhiteUnderAttackByBlackBishop implements UnderAttackCheck {
+
+    /**
+     * Checks that the piece at file {@code file} and rank {@code rank} is 
+     * threatened by a black bishop.
+     * 
+     * @param state the board state to consider.
+     * @param file  the file of the possibly threatened piece.
+     * @param rank  the rank of the possibly threatened piece.
+     * 
+     * @return {@code true} if and only if the piece in question is threatened.
+     */
+    @Override
+    public boolean check(final ChessBoardState state, 
+                         final int file, 
+                         final int rank) {
+    
+        if (whiteCellIsUnderAttackByBlackBishopNorthWest(state, file, rank)) {
+            return true;
+        }
+        
+        if (whiteCellIsUnderAttackByBlackBishopNorthEast(state, file, rank)) {
+            return true;
+        }
+        
+        if (whiteCellIsUnderAttackByBlackBishopSouthEast(state, file, rank)) {
+            return true;
+        }
+        
+        return whiteCellIsUnderAttackByBlackBishopSouthWest(state, file, rank);
+    }
+    
+    /**
+     * Checks that a white piece is under attack by a black bishop on the top
+     * left.
+     * 
+     * @param file the file of the piece to test for.
+     * @param rank the rank of the piece to test for.
+     * 
+     * @return {@code true} if and only if the white piece is under attack. 
+     */
+    private boolean whiteCellIsUnderAttackByBlackBishopNorthWest(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        int f = file - 1;
+        int r = rank - 1;
+        
+        while (f >= 0 && r >= 0) {
+            
+            final Piece piece = state.get(f, r);
+            
+            // Make sure that the counters are updated:
+            f--;
+            r--;
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.isWhite()) {
+                // Blocked by a piece with the same color (white):
+                return false;
+            }
+            
+            if (piece.getPieceType() == PieceType.BISHOP) {
+                // Match! The black queen threatens the piece:
+                return true;
+            }
+        }
+        
+        // Once here, there is no black queen below the piece:
+        return false;
+    }
+    
+    /**
+     * Checks that a white piece is under attack by a black bishop on the top
+     * right.
+     * 
+     * @param file the file of the piece to test for.
+     * @param rank the rank of the piece to test for.
+     * 
+     * @return {@code true} if and only if the white piece is under attack. 
+     */
+    private boolean whiteCellIsUnderAttackByBlackBishopNorthEast(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        int f = file + 1;
+        int r = rank - 1;
+        
+        while (f < N && r >= 0) {
+            
+            final Piece piece = state.get(f, r);
+            
+            // Make sure the indices are updated:
+            f++;
+            r--;
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.isWhite()) {
+                // Blocked by a piece with the same color:
+                return false;
+            }
+            
+            if (piece.getPieceType() == PieceType.BISHOP) {
+                // Match! The black queen threatens this position:
+                return true;
+            }
+        }
+        
+        // Once here, there is no black queen below the piece:
+        return false;
+    }
+    
+    /**
+     * Checks that a white piece is under attack by a black bishop on the bottom
+     * left.
+     * 
+     * @param file the file of the piece to test for.
+     * @param rank the rank of the piece to test for.
+     * 
+     * @return {@code true} if and only if the white piece is under attack. 
+     */
+    private boolean whiteCellIsUnderAttackByBlackBishopSouthWest(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        int f = file - 1;
+        int r = rank + 1;
+        
+        while (f >= 0 && r < N) {
+            
+            final Piece piece = state.get(f, r);
+            
+            // Make sure that the indices are updated:
+            f--;
+            r++;
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.isWhite()) {
+                // Blocked by the piece of the same color (white):
+                return false;
+            }
+            
+            if (piece.getPieceType() == PieceType.BISHOP) {
+                // Match! The black queen threatens the current position:
+                return true;
+            }
+        }
+        
+        // Once here, there is no black queen below the piece:
+        return false;
+    }
+    
+    /**
+     * Checks that a white piece is under attack by a black bishop on the bottom
+     * right.
+     * 
+     * @param file the file of the piece to test for.
+     * @param rank the rank of the piece to test for.
+     * 
+     * @return {@code true} if and only if the white piece is under attack. 
+     */
+    private boolean whiteCellIsUnderAttackByBlackBishopSouthEast(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        int f = file + 1;
+        int r = rank + 1;
+        
+        while (f < N && r < N) {
+            
+            final Piece piece = state.get(f, r);
+            
+            // Make sure that the indices are updated:
+            f++;
+            r++;
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.isWhite()) {
+                // Blocked by a piece with the same color (white):
+                return false;
+            }
+            
+            if (piece.getPieceType() == PieceType.BISHOP) {
+                // Match! A black queen threatens this piece:
+                return true;
+            }
+        }
+        
+        // Once here, there is no black queen below the piece:
+        return false;
+    }
+}
--- src/test/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackCheckByBlackBishopTest.java
+++ src/test/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackCheckByBlackBishopTest.java
@@ -6,22 +6,21 @@ import static com.github.coderodde.game.chess.PieceColor.BLACK;
 import static com.github.coderodde.game.chess.PieceColor.WHITE;
 import static com.github.coderodde.game.chess.PieceType.BISHOP;
 import static com.github.coderodde.game.chess.PieceType.PAWN;
-import static com.github.coderodde.game.chess.PieceType.QUEEN;
 import com.github.coderodde.game.chess.UnderAttackCheck;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public final class WhiteUnderAttackCheckByBlackQueenTest {
+public final class WhiteUnderAttackCheckByBlackBishopTest {
     
-    private static final Piece whiteQueen = new Piece(WHITE, QUEEN);
-    private static final Piece blackQueen = new Piece(BLACK, QUEEN);
+    private static final Piece whiteBishop = new Piece(WHITE, BISHOP);
+    private static final Piece blackBishop = new Piece(BLACK, BISHOP);
     
     private static final Piece whitePawn = new Piece(WHITE, PAWN);
     private static final Piece blackPawn = new Piece(BLACK, PAWN);
     
-    private static final UnderAttackCheck QUEEN_CHECK = 
-            new WhiteUnderAttackCheckByBlackQueen();
+    private static final UnderAttackCheck BISHOP_CHECK = 
+            new WhiteUnderAttackByBlackBishop();
     
     private final ChessBoardState state = new ChessBoardState();
     
@@ -32,98 +31,50 @@ public final class WhiteUnderAttackCheckByBlackQueenTest {
     }
     
     @Test
-    public void blackQueenNorth() {
-        state.set(3, 1, blackQueen);
-        assertCheck();
-    }
-    
-    @Test
-    public void blackQueenSouth() {
-        state.set(3, 7, blackQueen);
-        assertCheck();
-    }
-    
-    @Test
-    public void blackQueenWest() {
-        state.set(0, 3, blackQueen);
-        assertCheck();
-    }
-    
-    @Test
-    public void blackQueenEast() {
-        state.set(6, 3, blackQueen);
-        assertCheck();
-    }
-    
-    @Test
     public void blackQueenNorthEast() {
-        state.set(6, 0, blackQueen);
+        state.set(6, 0, blackBishop);
         assertCheck();
     }
     
     @Test
     public void blackQueenNorthWest() {
-        state.set(0, 0, blackQueen);
+        state.set(0, 0, blackBishop);
         assertCheck();
     }
     
     @Test
     public void blackQueenSouthEast() {
-        state.set(6, 6, blackQueen);
+        state.set(6, 6, blackBishop);
         assertCheck();
     }
     
     @Test
     public void blackQueenSouthWest() {
-        state.set(0, 6, blackQueen);
+        state.set(0, 6, blackBishop);
         assertCheck();
     }
     
     @Test
-    public void whiteQueenNorth() {
-        state.set(3, 1, whiteQueen);
-        assertNotCheck();
-    }
-    
-    @Test
-    public void whiteQueenSouth() {
-        state.set(3, 7, whiteQueen);
-        assertNotCheck();
-    }
-    
-    @Test
-    public void whiteQueenWest() {
-        state.set(0, 3, whiteQueen);
-        assertNotCheck();
-    }
-    
-    @Test
-    public void whiteQueenEast() {
-        state.set(6, 3, whiteQueen);
-        assertNotCheck();
-    }
-    
-    @Test
     public void whiteQueenNorthEast() {
-        state.set(6, 0, whiteQueen);
+        state.set(6, 0, whiteBishop);
         assertNotCheck();
     }
     
     @Test
     public void whiteQueenNorthWest() {
-        state.set(0, 0, whiteQueen);
+        state.set(0, 0, whiteBishop);
         assertNotCheck();
     }
     
     @Test
     public void whiteQueenSouthEast() {
-        state.set(6, 6, whiteQueen);
+        state.set(6, 6, whiteBishop);
         assertNotCheck();
     }
     
     @Test
     public void whiteQueenSouthWest() {
-        state.set(0, 6, whiteQueen);
+        state.set(0, 6, whiteBishop);
         assertNotCheck();
     }
         
@@ -180,18 +131,12 @@ public final class WhiteUnderAttackCheckByBlackQueenTest {
         state.set(3, 3, new Piece(WHITE, BISHOP));
         
         // Diagonal queens:
-        state.set(0, 0, blackQueen);
-        state.set(6, 6, blackQueen);
-        state.set(1, 5, blackQueen);
-        state.set(5, 1, blackQueen);
+        state.set(0, 0, blackBishop);
+        state.set(6, 6, blackBishop);
+        state.set(1, 5, blackBishop);
+        state.set(5, 1, blackBishop);
         
-        // Straight queens:
-        state.set(3, 5, blackQueen);
-        state.set(3, 0, blackQueen);
-        state.set(0, 3, blackQueen);
-        state.set(7, 3, blackQueen);
-        
-        assertTrue(QUEEN_CHECK.check(state, 3, 3));
+        assertTrue(BISHOP_CHECK.check(state, 3, 3));
     }
     
     @Test
@@ -220,21 +165,21 @@ public final class WhiteUnderAttackCheckByBlackQueenTest {
     
     @Test
     public void limitNorthEastRank() {
-        state.set(3, 6, whiteQueen);
+        state.set(3, 6, whiteBishop);
         assertNotCheck(3, 6);
     }
     
     private void assertNotCheck(final int file, 
                                 final int rank) {
         
-        assertFalse(QUEEN_CHECK.check(state, file, rank));
+        assertFalse(BISHOP_CHECK.check(state, file, rank));
     }
     
     private void assertCheck() {
-        assertTrue(QUEEN_CHECK.check(state, 3, 3));
+        assertTrue(BISHOP_CHECK.check(state, 3, 3));
     }
     
     private void assertNotCheck() {
-        assertFalse(QUEEN_CHECK.check(state, 3, 3));
+        assertFalse(BISHOP_CHECK.check(state, 3, 3));
     }
 }
