e5b6c210d68aab676a727d6744ff43c8982211e6
Rodion Efremov <coderodd3@yandex.com>
2024-07-14 18:29:09 +0300

    Both color king attack checks unit tested with 100% coverage.

--- src/main/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackCheckByBlackKing.java
+++ src/main/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackCheckByBlackKing.java
@@ -1,13 +1,313 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.github.coderodde.game.chess.impl.attackcheck;
 
+import com.github.coderodde.game.chess.ChessBoardState;
+import static com.github.coderodde.game.chess.ChessBoardState.N;
+import com.github.coderodde.game.chess.Piece;
+import static com.github.coderodde.game.chess.PieceType.KING;
+import com.github.coderodde.game.chess.UnderAttackCheck;
+
 /**
- *
- * @author rodio
+ * This class is responsible for checking whether a white piece is threatened by
+ * a black king.
+ * 
+ * @version 1.0.0 (Jul 12, 2024)
+ * @since 1.0.0 (Jul 12, 2024)
  */
-public class WhiteUnderAttackCheckByBlackKing {
+public final class WhiteUnderAttackCheckByBlackKing 
+        implements UnderAttackCheck {
+
+    /**
+     * Checks that a white piece at the file {@code file} and the rank 
+     * {@code rank} is threatened by the black king.
+     * 
+     * @param state the state to check in.
+     * @param file  the file of the black piece.
+     * @param rank  the rank of the black piece.
+     * 
+     * @return {@code true} if and only if the specified white piece is 
+     *         threatened by the black king.
+     */
+    @Override
+    public boolean check(final ChessBoardState state,
+                         final int file, 
+                         final int rank) {
+        if (whitePieceUnderAttackByBlackKingNorth(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingSouth(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingWest(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingEast(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingNorthEast(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingSouthEast(state, file, rank)) {
+            return true;
+        }
+        
+        if (whitePieceUnderAttackByBlackKingSouthWest(state, file, rank)) {
+            return true;
+        }
+        
+        return whitePieceUnderAttackByBlackKingNorthWest(state, file, rank);
+    }
     
+    /**
+     * Checks that a white piece is threatened by a black king at north.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingNorth(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == 0) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file, rank - 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+        
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at south.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingSouth(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == N - 1) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file, rank + 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white  piece is threatened by a black king at west.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingWest(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (file == 0) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file - 1, rank);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at east.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingEast(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (file == N - 1) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file + 1, rank);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at north east.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingNorthEast(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == 0) {
+            return false;
+        }
+        
+        if (file == N - 1) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file + 1, rank - 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at south east.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingSouthEast(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == N - 1) {
+            return false;
+        }
+        
+        if (file == N - 1) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file + 1, rank + 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at south west.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingSouthWest(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == N - 1) {
+            return false;
+        }
+        
+        if (file == 0) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file - 1, rank + 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
+    
+    /**
+     * Checks that a white piece is threatened by a black king at north west.
+     * 
+     * @param state the state to check.
+     * @param file  the file of a black piece to test.
+     * @param rank  the rank of a black piece to test.
+     * 
+     * @return {@code true} if and only if a white piece is threatened by a 
+     *         black king.
+     */
+    private boolean whitePieceUnderAttackByBlackKingNorthWest(
+            final ChessBoardState state,
+            final int file, 
+            final int rank) {
+        
+        if (rank == 0) {
+            return false;
+        }
+        
+        if (file == 0) {
+            return false;
+        }
+        
+        final Piece piece = state.get(file - 1, rank - 1);
+        
+        if (piece == null) {
+            // Nothing to threat:
+            return false;
+        }
+        
+        return piece.isBlack() && piece.getPieceType() == KING;
+    }
 }
---
+++ src/test/java/com/github/coderodde/game/chess/impl/attackcheck/WhiteUnderAttackCheckByBlackKingTest.java
@@ -0,0 +1,331 @@
+package com.github.coderodde.game.chess.impl.attackcheck;
+
+import com.github.coderodde.game.chess.ChessBoardState;
+import com.github.coderodde.game.chess.Piece;
+import static com.github.coderodde.game.chess.PieceColor.BLACK;
+import static com.github.coderodde.game.chess.PieceColor.WHITE;
+import static com.github.coderodde.game.chess.PieceType.KING;
+import static com.github.coderodde.game.chess.PieceType.PAWN;
+import com.github.coderodde.game.chess.UnderAttackCheck;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class WhiteUnderAttackCheckByBlackKingTest {
+    
+    private static final Piece whiteKing = new Piece(WHITE, KING);
+    private static final Piece blackKing = new Piece(BLACK, KING);
+    private static final Piece whitePawn = new Piece(WHITE, PAWN);
+    private static final Piece blackPawn = new Piece(BLACK, PAWN);
+    
+    private static final UnderAttackCheck KING_CHECK = 
+            new WhiteUnderAttackCheckByBlackKing();
+    
+    private final ChessBoardState state = new ChessBoardState();
+    
+    @Before
+    public void before() {
+        state.clear();
+        state.set(3, 3, whitePawn);
+    }
+    
+    @Test
+    public void blackKingNorthThreatens() {
+        state.set(3, 2, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingSouthThreatens() {
+        state.set(3, 4, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingWestThreatens() {
+        state.set(2, 3, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingEastThreatens() {
+        state.set(4, 3, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingNorthWestThreatens() {
+        state.set(2, 2, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingNorthEastThreatens() {
+        state.set(4, 2, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingSouthWestThreatens() {
+        state.set(2, 4, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blackKingSouthEastThreatens() {
+        state.set(4, 4, blackKing);
+        assertCheck();
+    }
+    
+    @Test
+    public void blockKingNorthThreatens() {
+        state.set(3, 2, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingSouthThreatens() {
+        state.set(3, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingWestThreatens() {
+        state.set(2, 3, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingEastThreatens() {
+        state.set(4, 3, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingNorthWestThreatens() {
+        state.set(2, 2, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingNorthEastThreatens() {
+        state.set(4, 2, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingSouthWestThreatens() {
+        state.set(2, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockKingSouthEastThreatens() {
+        state.set(4, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnNorthThreatens() {
+        state.set(3, 2, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnSouthThreatens() {
+        state.set(3, 4, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnWestThreatens() {
+        state.set(2, 3, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnEastThreatens() {
+        state.set(4, 3, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnNorthWestThreatens() {
+        state.set(2, 2, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnNorthEastThreatens() {
+        state.set(4, 2, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnSouthWestThreatens() {
+        state.set(2, 4, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnSouthEastThreatens() {
+        state.set(4, 4, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void blockPawnNorthNotThreatens() {
+        state.set(3, 2, blackPawn);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingSouthNotThreatens() {
+        state.set(3, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingWestNotThreatens() {
+        state.set(2, 3, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingEastNotThreatens() {
+        state.set(4, 3, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingNorthWestNotThreatens() {
+        state.set(2, 2, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingNorthEastNotThreatens() {
+        state.set(4, 2, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingSouthWestNotThreatens() {
+        state.set(2, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void whiteKingSouthEastNotThreatens() {
+        state.set(4, 4, whiteKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void northWestCorner() {
+        state.set(0, 0, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void northEastCorner() {
+        state.set(7, 0, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void southWestCorner() {
+        state.set(0, 7, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void soutEastCorner() {
+        state.set(7, 7, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void allSurroundingKings() {
+        state.set(2, 2, whiteKing);
+        state.set(3, 2, whiteKing);
+        state.set(4, 2, whiteKing);
+        state.set(2, 3, whiteKing);
+        state.set(4, 3, whiteKing);
+        state.set(2, 4, whiteKing);
+        state.set(3, 4, whiteKing);
+        state.set(4, 4, whiteKing);
+        
+        assertNotCheck();
+    }
+    
+    @Test
+    public void rank0() {
+        state.set(3, 0, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void rank7() {
+        state.set(3, 7, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void file0() {
+        state.set(0, 2, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void file7() {
+        state.set(7, 2, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void bottomCenterWhiteKing() {
+        state.set(2, 7, blackKing);
+        assertNotCheck();
+    }
+    
+    @Test
+    public void cornerCase1() {
+        assertNotCheck(1, 0, whitePawn);
+    }
+    
+    @Test
+    public void cornerCase2() {
+        assertNotCheck(1, 7, whitePawn);
+    }
+    
+    @Test
+    public void cornerCase3() {
+        assertNotCheck(0, 1, whitePawn);
+    }
+    
+    @Test
+    public void cornerCase4() {
+        assertNotCheck(7, 1, whitePawn);
+    }
+    
+    private void assertCheck() {
+        assertTrue(KING_CHECK.check(state, 3, 3));
+    }
+    
+    private void assertNotCheck() {
+        assertFalse(KING_CHECK.check(state, 3, 3));
+    }
+    
+    private void assertNotCheck(final int file, 
+                                final int rank, 
+                                final Piece piece) {
+        
+        state.set(file, 
+                  rank, 
+                  piece);
+        
+        assertFalse(KING_CHECK.check(state, file, rank));
+    }
+}
