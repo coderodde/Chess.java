c350073df57bb7e8a6b6fb052aaf983b793584e6
Rodion Efremov <coderodd3@yandex.com>
2024-08-06 11:10:13 +0300

    ShannonHeuristicFunction is filled. Need coverage.

--- src/main/java/com/github/coderodde/game/chess/impl/ShannonHeuristicFunction.java
+++ src/main/java/com/github/coderodde/game/chess/impl/ShannonHeuristicFunction.java
@@ -4,6 +4,10 @@ import com.github.coderodde.game.chess.ChessBoardState;
 import static com.github.coderodde.game.chess.ChessBoardState.N;
 import com.github.coderodde.game.chess.AbstractHeuristicFunction;
 import com.github.coderodde.game.chess.Piece;
+import com.github.coderodde.game.chess.PieceColor;
+import static com.github.coderodde.game.chess.PieceColor.BLACK;
+import static com.github.coderodde.game.chess.PieceColor.WHITE;
+import static com.github.coderodde.game.chess.PieceType.PAWN;
 import com.github.coderodde.game.chess.impl.attackcheck.BlackUnderAttackCheck;
 import com.github.coderodde.game.chess.impl.attackcheck.WhiteUnderAttackCheck;
 import com.github.coderodde.game.chess.UnderAttackCheck;
@@ -28,6 +32,16 @@ public final class ShannonHeuristicFunction extends AbstractHeuristicFunction {
     
     private final Map<ChessBoardState, Integer> stateFrequencyMap = 
             new HashMap<>();
+
+    @Override
+    public void clearStateFrequencyMap() {
+        this.stateFrequencyMap.clear();
+    }
+
+    @Override
+    public Map<ChessBoardState, Integer> getStateFrequencyMap() {
+        return stateFrequencyMap;
+    }
     
     @Override
     public double evaluate(final ChessBoardState state, final int depth) {
@@ -71,32 +85,296 @@ public final class ShannonHeuristicFunction extends AbstractHeuristicFunction {
     }
     
     private double pawnMaterial(final ChessBoardState state) {
-        throw new UnsupportedOperationException();
+        int score = 0;
+        
+        for (int file = 0; file < N; file++) {
+            score += countBlockedPawnWhite(state, file);
+            score -= countBlockedPawnBlack(state, file);
+            
+            score += countDoubledPawnsWhite(state, file);
+            score -= countDoubledPawnsBlack(state, file);
+            
+            score += countIsolatedPawnsWhite(state, file);
+            score -= countIsolatedPawnsBlack(state, file);
+        }
+        
+        return 0.5 * score;
     }
     
-    private double mobility(final ChessBoardState state) {
+    /**
+     * Counts the number of blocked pawns on file {@code file} in the state
+     * {@code state}.
+     * 
+     * @param state the state to investigate for the number of blocked pawns.
+     * @param file  the target file to investigate.
+     * 
+     * @return the count of blocked pawns at file {@code file} multiplied by 
+     *         0.5.
+     */
+    int countBlockedPawnWhite(final ChessBoardState state,
+                              final int file) {
+        
+        int blockedPawnCount = 0;
+        
+        for (int rank = N - 1; rank >= 1; rank--) {
+            final Piece currentPiece = state.get(file, rank);
+            
+            if (currentPiece == null) {
+                continue;
+            }
+            
+            if (currentPiece.getPieceType() != PAWN) {
+                continue;
+            }
+            
+            if (currentPiece.isBlack()) {
+                // Not a white pawn in question:
+                continue;
+            }
+            
+            final Piece nextPiece = state.get(file, rank - 1);
+            
+            if (nextPiece == null) {
+                continue;
+            }
+            
+            blockedPawnCount++;
+        }
+        
+        return blockedPawnCount;
+    }
+    
+    int countBlockedPawnBlack(final ChessBoardState state,
+                              final int file) {
+        
+        int blockedPawnCount = 0;
+        
+        for (int rank = 0; rank < N - 1; rank++) {
+            final Piece currentPiece = state.get(file, rank);
+            
+            if (currentPiece == null) {
+                continue;
+            }
+            
+            if (currentPiece.getPieceType() != PAWN) {
+                continue;
+            }
+            
+            if (currentPiece.isWhite()) {
+                // Not a white pawn in question:
+                continue;
+            }
+            
+            final Piece nextPiece = state.get(file, rank - 1);
+            
+            if (nextPiece == null) {
+                continue;
+            }
+            
+            blockedPawnCount++;
+        }
+        
+        return blockedPawnCount;
+    }
+    
+    int countDoubledPawnsWhite(final ChessBoardState state,
+                               final int file) {
+        int count = -1;
+        
+        for (int rank = 1; rank < N - 1; rank++) {
+            
+            final Piece piece = state.get(file, rank);
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.getPieceType() != PAWN) {
+                continue;
+            }
+            
+            if (piece.isWhite()) {
+                count++;
+            }
+        }
+        
+        return Math.max(0, count);
+    }
+    
+    int countDoubledPawnsBlack(final ChessBoardState state,
+                               final int file) {
+        int count = -1;
+        
+        for (int rank = 1; rank < N - 1; rank++) {
+            
+            final Piece piece = state.get(file, rank);
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.getPieceType() != PAWN) {
+                continue;
+            }
+            
+            if (piece.isBlack()) {
+                count++;
+            }
+        }
+        
+        return Math.max(0, count);
+    }
+    
+    double mobility(final ChessBoardState state) {
+        int mobilityWhite = 0;
+        int mobilityBlack = 0;
         
         for (int rank = 0; rank < N; rank++) {
             for (int file = 0; file < N; file++) {
                 
                 final Piece piece = state.get(file, rank);
                 
+                if (piece == null) {
+                    continue;
+                }
                 
+                if (piece.isWhite()) {
+                    for (int rank2 = 0; rank2 < N; rank2++) {
+                        for (int file2 = 0; file2 < N; file2++) {
+                            if (BLACK_CHECK.check(state, file2, rank2)) {
+                                mobilityWhite++;
+                            }
+                        }
+                    }
+                } else {
+                    for (int rank2 = 0; rank2 < N; rank2++) {
+                        for (int file2 = 0; file2 < N; file2++) {
+                            if (WHITE_CHECK.check(state, file2, rank2)) {
+                                mobilityBlack++;
+                            }
+                        }
+                    }
+                }
             }
         }
         
-        
-        
-        throw new UnsupportedOperationException();
+        return 0.1 * (mobilityBlack - mobilityWhite);       
     }
-
-    @Override
-    public void clearStateFrequencyMap() {
-        this.stateFrequencyMap.clear();
+    
+    private boolean[] getOpenFileFlags(final PieceColor color,
+                                       final ChessBoardState state) {
+        
+        final boolean[] openFileFlags = new boolean[N];
+        
+        for (int file = 0; file < N; file++) {
+            openFileFlags[file] = isFileOpen(color, state, file);
+        }
+        
+        return openFileFlags;
     }
+    
+    private boolean isFileOpen(final PieceColor color, 
+                               final ChessBoardState state,
+                               final int file) {
+        
+        for (int rank = 0; rank < N; rank++) {
+            final Piece piece = state.get(file, rank);
+            
+            if (piece == null) {
+                continue;
+            }
+            
+            if (piece.getPieceColor() == color &&
+                piece.getPieceType() == PAWN) {
+                
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private int countIsolatedPawnsImpl(final ChessBoardState state,
+                                       final int file,
+                                       final PieceColor pieceColor) {
+        int count = 0;
+        
+        final boolean[] openFlags = getOpenFileFlags(pieceColor, state);
+        
+        if (file == 0) {
+            for (int rank = 0; rank < N; rank++) {
+                final Piece piece = state.get(0, rank);
+                
+                if (piece == null) {
+                    continue;
+                }
+                
+                if (piece.getPieceType() != PAWN) {
+                    continue;
+                }
+                
+                if (piece.getPieceColor() == pieceColor) {
+                    if (openFlags[1]) {
+                        count++;
+                    }
+                }
+            }
+        } else if (file == N - 1) {
+            for (int rank = 0; rank < N; rank++) {
+                final Piece piece = state.get(N - 1, rank);
+                
+                if (piece == null) {
+                    continue;
+                }
+                
+                if (piece.getPieceType() != PAWN) {
+                    continue;
+                }
+                
+                if (piece.getPieceColor() == pieceColor) {
+                    if (openFlags[1]) {
+                        count++;
+                    }
+                }
+            }
+        } else {
+            for (int f = 1; f < N - 1; f++) {
+                for (int rank = 0; rank < N; rank++) {
+                    final Piece piece = state.get(N - 1, rank);
 
-    @Override
-    public Map<ChessBoardState, Integer> getStateFrequencyMap() {
-        return stateFrequencyMap;
+                    if (piece == null) {
+                        continue;
+                    }
+
+                    if (piece.getPieceType() != PAWN) {
+                        continue;
+                    }
+
+                    if (piece.getPieceColor() == pieceColor) {
+                        if (openFlags[f - 1] && openFlags[f + 1]) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    int countIsolatedPawnsWhite(final ChessBoardState state, 
+                                final int file) {
+        
+        return countIsolatedPawnsImpl(state,
+                                      file, 
+                                      WHITE);
+    }
+    
+    int countIsolatedPawnsBlack(final ChessBoardState state, 
+                                final int file) {
+        
+        return countIsolatedPawnsImpl(state, 
+                                      file,
+                                      BLACK);
     }
 }
--- src/test/java/com/github/coderodde/game/chess/impl/ShannonHeuristicFunctionTest.java
+++ src/test/java/com/github/coderodde/game/chess/impl/ShannonHeuristicFunctionTest.java
@@ -1,50 +1,173 @@
 package com.github.coderodde.game.chess.impl;
 
-import com.github.coderodde.game.chess.AbstractHeuristicFunction;
 import com.github.coderodde.game.chess.ChessBoardState;
 import com.github.coderodde.game.chess.Piece;
 import static com.github.coderodde.game.chess.PieceColor.BLACK;
 import static com.github.coderodde.game.chess.PieceColor.WHITE;
-import static com.github.coderodde.game.chess.PieceType.BISHOP;
-import static com.github.coderodde.game.chess.PieceType.KNIGHT;
 import static com.github.coderodde.game.chess.PieceType.PAWN;
-import org.junit.Before;
+import static com.github.coderodde.game.chess.PieceType.ROOK;
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 public final class ShannonHeuristicFunctionTest {
     
-    private static final ChessBoardState state = new ChessBoardState();
-
-    private final AbstractHeuristicFunction heuristicFunction = 
-            null;
+    private static final ShannonHeuristicFunction heuristicFunction = 
+            new ShannonHeuristicFunction();
+    
+    private final ChessBoardState state = new ChessBoardState();
+    
+    private static final double EPSILON = 0.00001;
+    private static final Piece blackRook = new Piece(BLACK, ROOK);
+    private static final Piece whitePawn = new Piece(WHITE, PAWN);
+    private static final Piece blackPawn = new Piece(BLACK, PAWN);
     
     @Before
-    public void before() {
+    public void beforeEach() {
         state.clear();
     }
     
     @Test
-    public void stateZeroScore() {
-        state.set(3, 3, new Piece(WHITE, BISHOP));
-        state.set(5, 5, new Piece(BLACK, BISHOP));
+    public void mobility1() {
+        double mobility = heuristicFunction.mobility(state);
         
-        assertEquals(0, heuristicFunction.evaluate(state, -1));
+        assertEquals(0.0, mobility, EPSILON);
+        
+        state.set(4, 4, blackRook);
+        state.set(2, 6, whitePawn);
+        
+        mobility = heuristicFunction.mobility(state);
+        
+        assertEquals(1.2, mobility, EPSILON);
     }
     
     @Test
-    public void avoidBlackAttack() {
-        state.set(3, 3, new Piece(WHITE, PAWN));
-        state.set(6, 6, new Piece(BLACK, KNIGHT));
+    public void countBlockedPawnWhite() {
+        state.set(3, 5, whitePawn);
+        state.set(3, 4, whitePawn);
+        state.set(3, 3, blackRook);
         
-        assertEquals(4, heuristicFunction.evaluate(state, 0));
+        state.set(1, 4, whitePawn);
+        state.set(1, 3, whitePawn);
+        state.set(1, 2, whitePawn);
+        state.set(1, 1, whitePawn);
+        
+        // Noise:
+        state.set(5, 4, blackPawn);
+        state.set(5, 4, blackPawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnWhite(state, 3));
+        assertEquals(3, heuristicFunction.countBlockedPawnWhite(state, 1));
     }
     
     @Test
-    public void avoidWhiteAttack() {
-        state.set(3, 3, new Piece(BLACK, PAWN));
-        state.set(6, 6, new Piece(WHITE, KNIGHT));
+    public void countBlockedPawnBlack() {
+        state.set(3, 5, blackPawn);
+        state.set(3, 4, blackPawn);
+        state.set(3, 3, blackRook);
         
-        assertEquals(-4, heuristicFunction.evaluate(state, 0));
+        state.set(1, 4, blackPawn);
+        state.set(1, 3, blackPawn);
+        state.set(1, 2, blackPawn);
+        state.set(1, 1, blackPawn);
+        
+        // Noise:
+        state.set(5, 4, whitePawn);
+        state.set(5, 4, whitePawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnBlack(state, 3));
+        assertEquals(3, heuristicFunction.countBlockedPawnBlack(state, 1));
+    }
+    
+    @Test
+    public void countDoubledPawnsWhiteNoDoublePawns() {
+        state.set(2, 2, whitePawn);
+        
+        assertEquals(0, heuristicFunction.countDoubledPawnsWhite(state, 2));
+        assertEquals(0, heuristicFunction.countDoubledPawnsWhite(state, 1));
+        assertEquals(0, heuristicFunction.countDoubledPawnsWhite(state, 3));
+    }
+    
+    @Test
+    public void countDoubledPawnsBlackNoDoublePawns() {
+        state.set(2, 2, blackPawn);
+        
+        assertEquals(0, heuristicFunction.countDoubledPawnsBlack(state, 2));
+        assertEquals(0, heuristicFunction.countDoubledPawnsBlack(state, 1));
+        assertEquals(0, heuristicFunction.countDoubledPawnsBlack(state, 3));
+    }
+    
+    @Test
+    public void countIsolatedPawnsWhiteFile0() {
+        state.set(0, 3, whitePawn);
+        
+        assertEquals(1, heuristicFunction.countIsolatedPawnsWhite(state, 0));
+    }
+    
+    @Test
+    public void countIsolatedPawnsWhiteFile7() {
+        state.set(7, 3, whitePawn);
+        
+        assertEquals(1, heuristicFunction.countIsolatedPawnsWhite(state, 7));
+    }
+    
+    @Test
+    public void countIsolatedPawnsWhiteFile0OnEmpty() {
+        assertEquals(0, heuristicFunction.countIsolatedPawnsWhite(state, 0));
+    }
+    
+    @Test
+    public void countIsolatedPawnsWhiteFile7OnEmpty() {
+        assertEquals(0, heuristicFunction.countIsolatedPawnsWhite(state, 7));
+    }
+    
+    @Test
+    public void countBlockedPawnsWhite() {
+        state.set(3, 5, whitePawn);
+        state.set(3, 4, whitePawn);
+        state.set(3, 3, whitePawn);
+        state.set(3, 2, whitePawn);
+        
+        assertEquals(0, heuristicFunction.countBlockedPawnWhite(state, 2));
+        assertEquals(3, heuristicFunction.countBlockedPawnWhite(state, 3));
+    
+        state.set(4, 5, whitePawn);
+        state.set(4, 4, whitePawn);
+        state.set(4, 3, whitePawn);
+        state.set(4, 1, whitePawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnWhite(state, 4));
+        
+        state.set(6, 6, whitePawn);
+        state.set(6, 5, whitePawn);
+        state.set(6, 2, whitePawn);
+        state.set(6, 1, whitePawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnWhite(state, 4));
+    }
+    
+    @Test
+    public void countBlockedPawnsBlack() {
+        state.set(3, 5, blackPawn);
+        state.set(3, 4, blackPawn);
+        state.set(3, 3, blackPawn);
+        state.set(3, 2, blackPawn);
+        
+        assertEquals(0, heuristicFunction.countBlockedPawnBlack(state, 2));
+        assertEquals(3, heuristicFunction.countBlockedPawnBlack(state, 3));
+    
+        state.set(4, 5, blackPawn);
+        state.set(4, 4, blackPawn);
+        state.set(4, 3, blackPawn);
+        state.set(4, 1, blackPawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnBlack(state, 4));
+        
+        state.set(6, 6, blackPawn);
+        state.set(6, 5, blackPawn);
+        state.set(6, 2, blackPawn);
+        state.set(6, 1, blackPawn);
+        
+        assertEquals(2, heuristicFunction.countBlockedPawnBlack(state, 4));
     }
 }
